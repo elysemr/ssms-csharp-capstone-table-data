f577ba1cced69c1e0c455f7793ed02d93e9f2cd4
Elyse Roth <elysemorganroth@gmail.com>
2021-10-27 14:59:08 -0400

    added csharp project data via ssms in scripts

---
+++ request-data-csharp-prs-db-sql.sql
@@ -0,0 +1,18 @@
+insert requests (Description, Justification, RejectionReason, DeliveryMode, Status, Total, UserId)
+VALUES
+('Coffee', 'Almost Out', NULL, 'PickUp', 'NEW', 0.00, 3),
+('Macbook', 'model is out of date', NULL, 'Delivery', 'NEW', 0.00, 6),
+('Calculator', 'accounting only has 1', NULL, 'Pickup', 'NEW', 0.00, 8),
+('Office', 'Need an updated version of excel', NULL, 'Electronic Delivery', 'NEW', 0.00, 7),
+('Keyboard', 'Carpal Tunnel', NULL, 'Delivery', 'NEW', 0.00, 1),
+('Desk', 'for my new office', NULL, 'Delivery', 'NEW', 0.00, 5),
+('Chair', 'to go with desk', NULL, 'Delivery', 'NEW', 0.00, 5),
+('Airpods', 'I need them', NULL, 'Delivery', 'NEW', 0.00, 5),
+('Headset', 'Need to go hands free', NULL, 'Pickup', 'NEW', 0.00, 9),
+('Monitor', 'I want to block Dwight''s head', NULL, 'Delivery', 'NEW', 0.00, 2),
+('Airpods', 'Gotta listen to my tunes', NULL, 'Delivery', 'NEW', 0.00, 10),
+('Mouse', 'need better mouse', NULL, 'Pickup', 'NEW', 0.00, 9),
+('Chair', 'mine is uncomfortable', NULL, 'Delivery', 'NEW', 0.00, 4),
+('Photoshop', 'you kno why', NULL, 'Download', 'NEW', 0.00, 1),
+('HDMI', 'Need to watch threat level midnight', NULL, 'Pickup', 'NEW', 0.00, 6),
+('Soda', 'Party!!!', NULL, 'Pickup', 'NEW', 0.00, 6);
---
+++ vendor-data-csharp-prs-db-sql.sql
@@ -0,0 +1,11 @@
+insert vendors (code, name, address, city, state, zip, phone, email)
+VALUES
+('APL', 'Apple', '1 Infinite Loop', 'Cupertino', 'CA', '95014', NULL, 'applecare@apple.com'),
+('MIC', 'Microsoft', 'One Microsoft Way', 'Redmond', 'WA', '98052', NULL, 'heysteve@microsoft.com'),
+('AMZ', 'Amazon', '410 Terry Ave North', 'Seattle', 'WA', '98109', '206-266-1000', 'heyjeff@amazon.com'),
+('TAR', 'Target', '1000 Nicollet Mall', 'Minneapolis', 'MI', '55403', '800-440-0680', 'bullseye@target.com'),
+('BBUY', 'Best Buy', '7601 Penn Avenue', 'South Richfield', 'MN', '55423', '888-237-8289', 'yellowtag@bestbuy.com'),
+('COST', 'Costco', '999 Lake Drive', 'Issaquah', 'WA', '98027', '425-313-8100', 'buylots@costco.com'),
+('IKEA', 'IKEA', '420 Alan Wood Road', 'Conshohocken', 'PA', '19428', NULL, 'buildit@ikea.com'),
+('KRO', 'Kroger', '1014 Vine St.', 'Cincinnati', 'OH', '45202', '800-576-4377', 'getfresh@kroger.com'),
+('ADO', 'Adobe', '345 Park Avenue', 'San Jose', 'CA', '95110', '408-536-6000', 'create@adobe.com');
