bab7dddbc37e2c6e34739cce9f1d7a7ddaeec321
Elyse Roth <82785689+elysemr@users.noreply.github.com>
2021-11-29 09:37:44 -0500

    Update user-data-csharp-prs-db.sql

--- user-data-csharp-prs-db.sql
+++ user-data-csharp-prs-db.sql
@@ -1,12 +1,12 @@
 insert Users (Username, Password, Firstname, Lastname, Phone, Email, IsReviewer, IsAdmin)
 VALUES
-('dschrute', '12345', 'Dwight', 'Schrute', '570-555-1234', 'dschrute@dundermiffline.com', 1, 0),
-('jhalpert', '12345', 'Jim', 'Halpert', '570-555-2345', 'jhalpert@dundermiffline.com', 1, 0),
-('pbeesly', '12345', 'Pam', 'Beesly', '570-555-3456', 'pbeesly@dundermiffline.com', 0, 0),
-('shudson', '12345', 'Stanley', 'Hudson', '570-555-4567', 'shudson@dundermiffline.com', 0, 0),
-('kkapoor', '12345', 'Kelly', 'Kapoor', '570-555-5678', 'kkapoor@dundermiffline.com', 0, 0),
-('mscott', '12345', 'Michael', 'Scott', '570-555-6789', 'mscott@dundermiffline.com', 1, 1),
-('tflenderson', '12345', 'Toby', 'Flenderson', '570-555-7890', 'tflenderson@dundermiffline.com', 1, 1),
-('omartinez', '12345', 'Oscar', 'Martinez', '570-555-8901', 'omartinez@dundermiffline.com', 0, 1),
-('amartin', '12345', 'Angela', 'Martin', '570-555-9012', 'amartin@dundermiffline.com', 0, 1),
-('abernard', '12345', 'Andy', 'Bernard', '570-555-0123', 'abernard@dundermiffline.com', 1, 0);
+('dschrute', '12345', 'Dwight', 'Schrute', '570-555-1234', 'dschrute@dundermifflin.com', 1, 0),
+('jhalpert', '12345', 'Jim', 'Halpert', '570-555-2345', 'jhalpert@dundermifflin.com', 1, 0),
+('pbeesly', '12345', 'Pam', 'Beesly', '570-555-3456', 'pbeesly@dundermifflin.com', 0, 0),
+('shudson', '12345', 'Stanley', 'Hudson', '570-555-4567', 'shudson@dundermifflin.com', 0, 0),
+('kkapoor', '12345', 'Kelly', 'Kapoor', '570-555-5678', 'kkapoor@dundermifflin.com', 0, 0),
+('mscott', '12345', 'Michael', 'Scott', '570-555-6789', 'mscott@dundermifflin.com', 1, 1),
+('tflenderson', '12345', 'Toby', 'Flenderson', '570-555-7890', 'tflenderson@dundermifflin.com', 1, 1),
+('omartinez', '12345', 'Oscar', 'Martinez', '570-555-8901', 'omartinez@dundermifflin.com', 0, 1),
+('amartin', '12345', 'Angela', 'Martin', '570-555-9012', 'amartin@dundermifflin.com', 0, 1),
+('abernard', '12345', 'Andy', 'Bernard', '570-555-0123', 'abernard@dundermifflin.com', 1, 0);
